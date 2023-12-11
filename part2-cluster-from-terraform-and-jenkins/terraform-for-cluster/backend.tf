5a5d74df1484c93c6ec90bc1ac91f1cde9d3bdd6
Vani1B <135303019+Vani1B@users.noreply.github.com>
2023-12-11 00:13:30 +0000

    Update backend.tf

--- part2-cluster-from-terraform-and-jenkins/terraform-for-cluster/backend.tf
+++ part2-cluster-from-terraform-and-jenkins/terraform-for-cluster/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "vani079093-sre-cicd-terraform-eks"
+    bucket = "vani0790-sre-cicd-terraform-eks"
     region = "us-east-1"
     key    = "eks/terraform.tfstate"
   }
